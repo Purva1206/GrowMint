b3ce447175e7de4c6054edbd8e56eaf7585d9e26
Purva1206 <purvarenge@gmail.com>
2026-07-20 09:38:41 +0530

    Update about.php

--- about.php
+++ about.php
@@ -312,6 +312,15 @@ body.dark-mode img {
 </style>
 
 </head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-073DKNF60S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-073DKNF60S');
+</script>
 
 <body>
 
