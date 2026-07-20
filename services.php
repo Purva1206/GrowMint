2858a95101133a4ada294e27cb759cf6150f04a9
Purva1206 <purvarenge@gmail.com>
2026-07-20 09:40:27 +0530

    Integrate Google Analytics tracking code
    Added Google Analytics tracking code to services.php

--- services.php
+++ services.php
@@ -234,7 +234,15 @@
     }
   </style>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-073DKNF60S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-073DKNF60S');
+</script>
 <body>
 
 <!-- NAVBAR from about.php -->
