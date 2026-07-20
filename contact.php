dbd6e66efa4d1dfd5ec0b23a689136d5fe8eb05c
Purva1206 <purvarenge@gmail.com>
2026-07-20 09:39:30 +0530

    Integrate Google Analytics into contact.php
    Added Google Analytics tracking code to contact page.

--- contact.php
+++ contact.php
@@ -158,6 +158,15 @@ body.dark-mode .contact-info a:hover {
 }
 </style>
 </head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-073DKNF60S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-073DKNF60S');
+</script>
 <body>
 
 <nav class="nav">
